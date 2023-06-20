9ee6b32ceceb69192791e4c7ea9689c6ebff2aee
giegiewealth <angelamarfo17@gmail.com>
2023-06-20 15:26:29 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
+/**
+ * main - Prints _putchar followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	int i;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	for (i = 0; str[i] != '\0'; i++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(str[i]);
+		_putchar(str[count]);
 	}
-
+	_putchar('\n');
 	return (0);
 }
 
